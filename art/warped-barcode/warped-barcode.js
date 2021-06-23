3f017839e518ec70c18acab99398fa820290fbe1
Max Philips <mphilips890@gmail.com>
2021-06-22 23:57:42 -0500

    Touch up warped-barcode

--- art/warped-barcode/warped-barcode.js
+++ art/warped-barcode/warped-barcode.js
@@ -1,29 +1,31 @@
 function setup() {
   createCanvas(640, 400);
   noLoop();
+
+  strokeCap(SQUARE);
 }
 
 function draw() {
   background(220);
 
-  // line(0, 0, 20, 390);
-  // line(-40, 10, 40, 380);
+  var x1 = 80, y1 = 360, x2 = 80, y2 = 40;
 
-  var x1 = -640, y1 = -40;
-  var x2 = 0, y2 = 440;
-  var deltaX1 = 16, deltaX2 = 8;
-  var color = randInt(255);
-
-  for (var i = 0; i < 81; i++) {
+  for (var i = 0; i < 60; i++) {
     line(x1, y1, x2, y2);
-    stroke(color);
+    strokeWeight(random([2, 4]));
 
-    x1 = x1 + (deltaX1 + (Math.round(randInt()) * 2 - 1) * Math.floor(randInt(2)));
-    x2 = x2 + (deltaX2 + (Math.round(randInt()) * 2 - 1) * Math.floor(randInt()));
-    
-    y1 = y1 + (Math.round(randInt()) * 2 - 1) * Math.floor(randInt(8));
-    y2 = y2 + (Math.round(randInt()) * 2 - 1) * Math.floor(randInt(4));
-    
-    color = randInt() * 255;
+    x1 = x1 + random(4, 12);
+    x2 = x2 + random(4, 12);
   }
+
+  push();
+  noStroke();
+  fill(220);
+  rect(80, 20, width - 80, 20);
+  rect(80, 360, width - 80, 20);
+  pop();
+}
+
+function section(code, x, y) {
+  // implement this to make the barcode real
 }
